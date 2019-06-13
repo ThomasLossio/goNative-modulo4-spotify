42702a2656a27e869ff49d4bbb2332d924d53f70
thomasneo <thomaslossio@hotmail.com>
2019-06-12 23:13:46 -0300

    Intial configurations, styling main page and initilizing Redux Sauce and Immutable, but not functionable yet. I'm going to fix soon

---
+++ src/pages/Main/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionsCreators } from 'redux';
+import PodcastsActions from '~/store/ducks/podcasts';
+
+import {
+  Container, PodcastList, PageTitle, Podcast, Cover, Info, Title, Count,
+} from './styles';
+
+class Main extends Component {
+  componentDidMount() {
+    const { loadRequest } = this.props;
+
+    loadRequest();
+  }
+
+  render() {
+    const { podcasts } = this.props;
+    return (
+      <Container>
+        <PodcastList
+          ListHeaderComponent={() => <PageTitle>Podcasts</PageTitle>}
+          data={podcasts.data}
+          keyExtractor={podcast => String(podcast.id)}
+          renderItem={({ item: podcast }) => (
+            <Podcast onPress={() => {}}>
+              <Cover source={{ uri: podcast.cover }} />
+              <Info>
+                <Title>{podcast.title}</Title>
+                <Count>{`${podcast.tracks.length} episódios`}</Count>
+              </Info>
+            </Podcast>
+          )}
+        />
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  podcasts: state.podcasts,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(PodcastsActions, dispatch);
+
+export default connect()(Main);
